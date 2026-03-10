5e2bc0c5d19bb9e1180f3e6b470d565ff002ab14
Dakota St. Laurent <d.h.stlaurent@gmail.com>
2026-03-10 13:03:32 -0400

    fmt

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { Wizard } from './components/Wizard'
-import './App.css'
+import { Wizard } from "./components/Wizard";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="app">
-      <h1>Multi-Step Wizard</h1>
-      <Wizard />
-    </div>
-  )
+	return (
+		<div className="app">
+			<h1>Multi-Step Wizard</h1>
+			<Wizard />
+		</div>
+	);
 }
 
-export default App
+export default App;
--- src/types.ts
+++ src/types.ts
@@ -1,17 +1,17 @@
 export type PersonalInfoFields = {
-  firstName: string
-  lastName: string
-  email: string
-}
+	firstName: string;
+	lastName: string;
+	email: string;
+};
 
 export type AddressFields = {
-  street: string
-  city: string
-  state: string
-  zip: string
-}
+	street: string;
+	city: string;
+	state: string;
+	zip: string;
+};
 
 export type WizardFormValues = {
-  personalInfo: PersonalInfoFields
-  address: AddressFields
-}
+	personalInfo: PersonalInfoFields;
+	address: AddressFields;
+};
